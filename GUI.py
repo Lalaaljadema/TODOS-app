7e2fb4ff728615f48ab94e21fd23b84b4fc09e9b
lalaaljadema <salsabilaaljadema9@gmail.com>
2023-02-14 09:21:40 +0700

    implement column in layout, and exit

--- GUI.py
+++ GUI.py
@@ -1,23 +1,28 @@
 import functions
 import PySimpleGUI as sg
 
-label= sg.Text("Type in a todo: ")   # Create a label on the  window
-input_box= sg.InputText(tooltip="Enter todo", key='todo')
-add_button= sg.Button("Add")
-list_todos= sg.Listbox(values=functions.get_todos('todos.txt'), key='todos',
-                     enable_events=True,size=[45,10])
-list_completed= sg.Listbox(values=functions.get_todos('completed_todos.txt'), key='c_todos',
-                      enable_events=True, size=[45,10])
-edit_button=sg.Button("Edit")
-delete_button=sg.Button("Delete")
-completed_button=sg.Button("Completed")
+label = sg.Text("Type in a todo: ")   # Create a label on the  window
+input_box = sg.InputText(tooltip = "Enter todo", key='todo')
+add_button = sg.Button("Add")
+list_todos = sg.Listbox(values = functions.get_todos('todos.txt'), key = 'todos',
+                     enable_events = True,size = [45,10])
+list_completed = sg.Listbox(values = functions.get_todos('completed_todos.txt'), key = 'c_todos',
+                      enable_events = True, size = [45,10])
+edit_button = sg.Button("Edit")
+delete_button = sg.Button("Delete")
+completed_button = sg.Button("Completed")
+left_column_content=[[list_todos],[list_completed]]
+right_column_content=[[edit_button],[delete_button],[completed_button]]
+left_column= sg.Column(left_column_content)
+right_column= sg.Column(right_column_content)
+exit_button=sg.Button("Exit")
 
-
-window= sg.Window('My To-Do App',
-                  layout=[[label, input_box, add_button],[list_todos,edit_button,delete_button,completed_button],[list_completed]],
-                  font=('Helvetica',15))
+window = sg.Window('My To-Do App',
+                  layout = [[label, input_box, add_button],[left_column,right_column],
+                            [exit_button]],
+                  font = ('Helvetica',15))
 while True:
-    event, values= window.read()
+    event, values = window.read()
     print(event)
     print(values)
     match event:
@@ -35,6 +40,8 @@ while True:
             todos[index]= new_todo
             functions.write_todos('todos.txt',todos)
             window['todos'].update(values=todos)  # window [todos] itu mengakses list box dari window karna todos itu key dari listbox
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])  #biar bikin yg diklik muncul di input text (todo)
         case 'Delete':
             todo_to_delete= values['todos'][0]
             todos= functions.get_todos('todos.txt')
@@ -52,7 +59,10 @@ while True:
             c_todos.append(completed_todos)
             functions.write_todos('completed_todos.txt',c_todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
             window['c_todos'].update(values=c_todos)
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
             break
 
